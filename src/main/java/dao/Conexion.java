52a1b1bc40800c76d30e6ac1f0aa38fd2f0d202f
Grajaal <jorgegrajal@gmail.com>
2024-01-31 12:27:27 +0100

    Actualizada la clase conexión para la base de datos remota

--- src/main/java/dao/Conexion.java
+++ src/main/java/dao/Conexion.java
@@ -1,11 +1,14 @@
 package dao;
+
 import java.sql.*;
 
 public class Conexion {
-    //Conexión Local
-    public static Connection conectar(){
+    // Conexión Local
+    public static Connection conectar() {
         try {
-            Connection cn = DriverManager.getConnection("jdbc:mysql://localhost/topfilms", "root", "");
+            Connection cn = DriverManager.getConnection(
+                    "jdbc:db-inso-do-user-15730192-0.c.db.ondigitalocean.com:25060/topfilms", "doadmin",
+                    "AVNS_UVPIJd_Aw8gJZd-Bxed");
             return cn;
         } catch (SQLException e) {
             System.out.println("Error en conexión local " + e);
