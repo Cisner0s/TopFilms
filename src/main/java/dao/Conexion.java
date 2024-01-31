f66c589e671108ff7264c080580478e773db874a
Cisner0s <dcisnm00@estudiantes.unileon.es>
2024-01-31 17:44:24 +0100

    Arreglada la conexion a la base de datos remota.

--- src/main/java/dao/Conexion.java
+++ src/main/java/dao/Conexion.java
@@ -6,12 +6,15 @@ public class Conexion {
     // Conexión Local
     public static Connection conectar() {
         try {
-            Connection cn = DriverManager.getConnection(
-                    "jdbc:db-inso-do-user-15730192-0.c.db.ondigitalocean.com:25060/topfilms", "doadmin",
-                    "AVNS_UVPIJd_Aw8gJZd-Bxed");
+            String url = "jdbc:mysql://db-inso-do-user-15730192-0.c.db.ondigitalocean.com:25060/topfilms";
+            String usuario = "doadmin";
+            String contraseña = "AVNS_UVPIJd_Aw8gJZd-Bxed";
+            
+            Connection cn = DriverManager.getConnection(url, usuario, contraseña);
             return cn;
+            
         } catch (SQLException e) {
-            System.out.println("Error en conexión local " + e);
+            System.out.println("Error en conexión remota " + e);
         }
         return (null);
     }
