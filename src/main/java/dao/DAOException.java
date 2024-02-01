6cdd80edb42ecba82586634deac210f0ee19fa04
Grajaal <jorgegrajal@gmail.com>
2024-02-01 20:58:59 +0100

    Nada importante

--- src/main/java/dao/DAOException.java
+++ src/main/java/dao/DAOException.java
@@ -1,6 +1,5 @@
 package dao;
 
-<<<<<<< HEAD
 public class DAOException extends Exception {
 
     public DAOException(String message) {
@@ -13,16 +12,5 @@ public class DAOException extends Exception {
 
     public DAOException(Throwable cause) {
         super(cause);
-=======
-/**
- *
- * @author jorge
- */
-public class DAOException {
-
-    DAOException() {
-        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
->>>>>>> 3c55a0a0ed887d78d378b723ffc5e7a7d7e56956
     }
-    
 }
