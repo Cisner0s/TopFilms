69bc2cd6fb13c6112ad8b1f30f488b6c57c3f401
francarreram <91256788+francarreram@users.noreply.github.com>
2024-02-05 20:06:28 +0100

    Controlador gestión usuarios

---
+++ src/main/java/controller/GestionUsuariosController.java
@@ -0,0 +1,194 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package controller;
+
+import dao.Conexion;
+import dao.DAOException;
+import dao.UsuarioDAO;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.swing.JButton;
+import javax.swing.JOptionPane;
+import model.Usuario;
+import model.EstatusUsuarios;
+import static model.EstatusUsuarios.ACTIVO;
+import static model.EstatusUsuarios.INACTIVO;
+import model.RolUsuarios;
+import static model.RolUsuarios.USUARIO;
+import static model.RolUsuarios.CRITICO;
+import static model.RolUsuarios.ADMIN;
+import view.GestionUsuariosWindow;
+
+/**
+ *
+ * @author fran
+ */
+public class GestionUsuariosController implements ActionListener {
+    private final GestionUsuariosWindow gestionUsuariosWindow;
+    private final UsuarioDAO dao;
+    public String username;
+    public String id;
+    public String u;
+    public String pswd;
+    public String name;
+    public RolUsuarios roleS;
+    public int role;
+    public EstatusUsuarios statusS;
+    public String status;
+    
+    public GestionUsuariosController(GestionUsuariosWindow gestionUsuariosWindow) {
+        this.gestionUsuariosWindow = gestionUsuariosWindow;
+        this.dao = new UsuarioDAO(Conexion.conectar());
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        username = gestionUsuariosWindow.jTextField22.getText().trim();
+        id = gestionUsuariosWindow.jTextField23.getText().trim();
+        u = gestionUsuariosWindow.jTextField21.getText().trim();
+        pswd = gestionUsuariosWindow.jTextField34.getText().trim();
+        name = gestionUsuariosWindow.jTextField35.getText();
+        status = gestionUsuariosWindow.jTextField38.getText().trim();
+        role = gestionUsuariosWindow.jComboBox_Rol.getSelectedIndex();
+        
+        if ("Activo".equals(status)){
+            statusS = ACTIVO;
+        } else if ("Inactivo".equals(status)) {
+            statusS = INACTIVO;
+        } else {
+            JOptionPane.showMessageDialog(null, "El estado debe ser 'Activo' o 'Inactivo'.");
+        }
+        
+        switch (role) {
+                case 0 -> roleS = USUARIO;
+                case 1 -> roleS = CRITICO;
+                case 2 -> roleS = ADMIN;
+                default -> {
+            }
+        }
+        
+        if (e.getSource() instanceof JButton) {
+            JButton botonClicado = (JButton) e.getSource();
+            if (botonClicado.equals(gestionUsuariosWindow.jButton4)){
+                try {
+                    botonCrear();
+                } catch (DAOException ex) {
+                    Logger.getLogger(GestionUsuariosController.class.getName()).log(Level.SEVERE, null, ex);
+                }
+            } else if (botonClicado.equals(gestionUsuariosWindow.jButton5)){
+                try {
+                    botonUsername();
+                } catch (DAOException ex) {
+                    Logger.getLogger(GestionUsuariosController.class.getName()).log(Level.SEVERE, null, ex);
+                }
+            } else if (botonClicado.equals(gestionUsuariosWindow.jButton6)) {
+                try {
+                    botonEditar();
+                } catch (DAOException ex) {
+                    Logger.getLogger(GestionUsuariosController.class.getName()).log(Level.SEVERE, null, ex);
+                }
+            } else if (botonClicado.equals(gestionUsuariosWindow.jButton7)) {
+                try {
+                    botonBorrar();
+                } catch (DAOException ex) {
+                    Logger.getLogger(GestionUsuariosController.class.getName()).log(Level.SEVERE, null, ex);
+                }
+            } else if (botonClicado.equals(gestionUsuariosWindow.jButton8)) {
+                try {
+                    botonID();
+                } catch (DAOException ex) {
+                    Logger.getLogger(GestionUsuariosController.class.getName()).log(Level.SEVERE, null, ex);
+                }
+            }
+        }
+    }
+    
+    public void botonCrear() throws DAOException {
+        if (!"".equals(u) && !"".equals(pswd) && !"".equals(u) && !"".equals(status)) {
+            Usuario user = new Usuario(u, pswd, name, roleS, statusS);
+            dao.create(user);
+        } else {
+            JOptionPane.showMessageDialog(null, "Debes llenar todos los campos");
+        }
+    }
+    
+    public void botonUsername() throws DAOException {
+        if (!"".equals(username)) {
+            Usuario user = dao.get(username);
+            gestionUsuariosWindow.jTextField21.setText(user.getNickName());
+            gestionUsuariosWindow.jTextField34.setText(user.getContraseña());
+            gestionUsuariosWindow.jTextField35.setText(user.getNombreCompleto());
+            if (user.getEstatus().equals(ACTIVO)){
+                gestionUsuariosWindow.jTextField38.setText("Activo");
+            } else if (user.getEstatus().equals(INACTIVO)) {
+                gestionUsuariosWindow.jTextField38.setText("Inactivo");
+            }
+        
+            if (user.getRol().equals(0)){ 
+                gestionUsuariosWindow.jComboBox_Rol.setSelectedIndex(0);
+            } else if (user.getRol().equals(1)) {
+                gestionUsuariosWindow.jComboBox_Rol.setSelectedIndex(1);
+            } else if (user.getRol().equals(2)) {
+                gestionUsuariosWindow.jComboBox_Rol.setSelectedIndex(2);
+            }
+        }
+    }
+    
+    public void botonEditar() throws DAOException {
+        if (!"".equals(u) && !"".equals(pswd) && !"".equals(u) && !"".equals(status)) {
+            Usuario user = new Usuario(u, pswd, name, roleS, statusS);
+            dao.update(user);
+        } else {
+            JOptionPane.showMessageDialog(null, "Debes llenar todos los campos");
+        }
+    }
+    
+    public void botonBorrar() throws DAOException {
+        int idInt;
+        if (!"".equals(id)) {
+            try {
+                idInt = Integer.parseInt(id);
+                dao.delete(idInt);
+            } catch (NumberFormatException e) {
+                JOptionPane.showMessageDialog(null, "Debes llenar todos los campos");
+            }
+        } else {
+            JOptionPane.showMessageDialog(null, "Debes llenar todos los campos");
+        }
+    }
+    
+    public void botonID() throws DAOException {
+        int idInt;
+        if (!"".equals(id)) {
+            try {
+                idInt = Integer.parseInt(id);
+                Usuario user = dao.get(idInt);
+                gestionUsuariosWindow.jTextField22.setText(user.getNickName());
+                gestionUsuariosWindow.jTextField21.setText(user.getNickName());
+                gestionUsuariosWindow.jTextField34.setText(user.getContraseña());
+                gestionUsuariosWindow.jTextField35.setText(user.getNombreCompleto());
+                if (user.getEstatus().equals(ACTIVO)){
+                    gestionUsuariosWindow.jTextField38.setText("Activo");
+                } else if (user.getEstatus().equals(INACTIVO)) {
+                    gestionUsuariosWindow.jTextField38.setText("Inactivo");
+                }
+        
+                if (user.getRol().equals(0)){ 
+                    gestionUsuariosWindow.jComboBox_Rol.setSelectedIndex(0);
+                } else if (user.getRol().equals(1)) {
+                    gestionUsuariosWindow.jComboBox_Rol.setSelectedIndex(1);
+                } else if (user.getRol().equals(2)) {
+                    gestionUsuariosWindow.jComboBox_Rol.setSelectedIndex(2);
+                }
+            } catch (NumberFormatException e) {
+                JOptionPane.showMessageDialog(null, "Debes llenar todos los campos");
+            }
+        } else {
+            JOptionPane.showMessageDialog(null, "Debes llenar todos los campos");
+        }
+    }
+}
