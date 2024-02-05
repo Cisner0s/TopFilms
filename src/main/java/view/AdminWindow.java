9a6c9f9f8e2576e9a1e0c37ef9467840604a36f9
Cisner0s <dcisnm00@estudiantes.unileon.es>
2024-02-05 19:26:55 +0100

    Creada una clase main y modificaciones para que sea correcta.

--- src/main/java/view/AdminWindow.java
+++ src/main/java/view/AdminWindow.java
@@ -28,11 +28,11 @@ public class AdminWindow extends javax.swing.JFrame {
         setLocationRelativeTo(null); 
         setIconImage(getIconImage());
         
-        AdminController ctr = new AdminController(this);
-        jButton_GestionReseñas.addActionListener(ctr);
-        jButton_GestionUsuarios.addActionListener(ctr);
-        jButton_GestionContenido.addActionListener(ctr);
-        jButton_CerrarSesion.addActionListener(ctr);
+        AdminController adminCtr = new AdminController(this);
+        jButton_GestionReseñas.addActionListener(adminCtr);
+        jButton_GestionUsuarios.addActionListener(adminCtr);
+        jButton_GestionContenido.addActionListener(adminCtr);
+        jButton_CerrarSesion.addActionListener(adminCtr);
     }
     
      @Override
@@ -105,43 +105,8 @@ public class AdminWindow extends javax.swing.JFrame {
         pack();
     }// </editor-fold>//GEN-END:initComponents
 
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String args[]) {
-        /* Set the Nimbus look and feel */
-        //<editor-fold defaultstate="collapsed" desc=" Look and feel setting code (optional) ">
-        /* If Nimbus (introduced in Java SE 6) is not available, stay with the default look and feel.
-         * For details see http://download.oracle.com/javase/tutorial/uiswing/lookandfeel/plaf.html 
-         */
-        try {
-            for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()) {
-                if ("Nimbus".equals(info.getName())) {
-                    javax.swing.UIManager.setLookAndFeel(info.getClassName());
-                    break;
-                }
-            }
-        } catch (ClassNotFoundException ex) {
-            java.util.logging.Logger.getLogger(AdminWindow.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        } catch (InstantiationException ex) {
-            java.util.logging.Logger.getLogger(AdminWindow.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        } catch (IllegalAccessException ex) {
-            java.util.logging.Logger.getLogger(AdminWindow.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        } catch (javax.swing.UnsupportedLookAndFeelException ex) {
-            java.util.logging.Logger.getLogger(AdminWindow.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        }
-        //</editor-fold>
 
-        /* Create and display the form */
-        java.awt.EventQueue.invokeLater(new Runnable() {
-            public void run() {
-                AdminWindow view = new AdminWindow();
-                view.setVisible(true);
-                AdminController admCtr = new AdminController(view);
-            }
-        });
-    }
-
+   
     // Variables declaration - do not modify//GEN-BEGIN:variables
     public javax.swing.JButton jButton_CerrarSesion;
     public javax.swing.JButton jButton_GestionContenido;
