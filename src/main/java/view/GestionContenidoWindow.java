c38997df5c0c7a34f19a833da916a34122b2a7a7
francarreram <91256788+francarreram@users.noreply.github.com>
2024-02-06 19:16:11 +0100

    Ventana gestión contenido

--- src/main/java/view/GestionContenidoWindow.java
+++ src/main/java/view/GestionContenidoWindow.java
@@ -4,6 +4,7 @@
  */
 package view;
 
+import controller.GestionContenidoController;
 import java.awt.Image;
 import java.awt.Toolkit;
 
@@ -22,6 +23,13 @@ public class GestionContenidoWindow extends javax.swing.JFrame {
         setTitle("Gestion de Contenido");
         setLocationRelativeTo(null);
         setIconImage(getIconImage());
+        
+        GestionContenidoController ctr = new GestionContenidoController(this);
+        jButton1.addMouseListener(ctr);
+        jCheckBox_Editar.addMouseListener(ctr);
+        jCheckBox_Anadir.addMouseListener(ctr);
+        jCheckBox_Borrar.addMouseListener(ctr);
+        jComboBox1.addActionListener(ctr);
     }
 
      @Override
@@ -55,11 +63,6 @@ public class GestionContenidoWindow extends javax.swing.JFrame {
 
         jComboBox1.setFont(new java.awt.Font("Segoe UI", 0, 14)); // NOI18N
         jComboBox1.setModel(new javax.swing.DefaultComboBoxModel<>(new String[] { "...", "Pelicula", "Serie", "Actor", "Director", "Estudio" }));
-        jComboBox1.addActionListener(new java.awt.event.ActionListener() {
-            public void actionPerformed(java.awt.event.ActionEvent evt) {
-                jComboBox1ActionPerformed(evt);
-            }
-        });
         jPanel2.add(jComboBox1, new org.netbeans.lib.awtextra.AbsoluteConstraints(110, 60, 290, -1));
 
         jLabel1.setFont(new java.awt.Font("Cambria", 0, 18)); // NOI18N
@@ -71,44 +74,24 @@ public class GestionContenidoWindow extends javax.swing.JFrame {
         jCheckBox_Editar.setForeground(new java.awt.Color(255, 255, 255));
         jCheckBox_Editar.setText("Editar");
         jCheckBox_Editar.setEnabled(false);
-        jCheckBox_Editar.addMouseListener(new java.awt.event.MouseAdapter() {
-            public void mouseClicked(java.awt.event.MouseEvent evt) {
-                jCheckBox_EditarMouseClicked(evt);
-            }
-        });
         jPanel2.add(jCheckBox_Editar, new org.netbeans.lib.awtextra.AbsoluteConstraints(220, 110, -1, -1));
 
         jCheckBox_Anadir.setFont(new java.awt.Font("Cambria", 0, 16)); // NOI18N
         jCheckBox_Anadir.setForeground(new java.awt.Color(255, 255, 255));
         jCheckBox_Anadir.setText("Añadir");
         jCheckBox_Anadir.setEnabled(false);
-        jCheckBox_Anadir.addMouseListener(new java.awt.event.MouseAdapter() {
-            public void mouseClicked(java.awt.event.MouseEvent evt) {
-                jCheckBox_AnadirMouseClicked(evt);
-            }
-        });
         jPanel2.add(jCheckBox_Anadir, new org.netbeans.lib.awtextra.AbsoluteConstraints(110, 110, -1, 20));
 
         jCheckBox_Borrar.setFont(new java.awt.Font("Cambria", 0, 16)); // NOI18N
         jCheckBox_Borrar.setForeground(new java.awt.Color(255, 255, 255));
         jCheckBox_Borrar.setText("Borrar");
         jCheckBox_Borrar.setEnabled(false);
-        jCheckBox_Borrar.addMouseListener(new java.awt.event.MouseAdapter() {
-            public void mouseClicked(java.awt.event.MouseEvent evt) {
-                jCheckBox_BorrarMouseClicked(evt);
-            }
-        });
         jPanel2.add(jCheckBox_Borrar, new org.netbeans.lib.awtextra.AbsoluteConstraints(320, 110, -1, -1));
 
         jButton1.setFont(new java.awt.Font("Cambria", 0, 16)); // NOI18N
         jButton1.setIcon(new javax.swing.ImageIcon(getClass().getResource("/images/ir_1.png"))); // NOI18N
         jButton1.setBorderPainted(false);
         jButton1.setContentAreaFilled(false);
-        jButton1.addMouseListener(new java.awt.event.MouseAdapter() {
-            public void mouseClicked(java.awt.event.MouseEvent evt) {
-                jButton1MouseClicked(evt);
-            }
-        });
         jPanel2.add(jButton1, new org.netbeans.lib.awtextra.AbsoluteConstraints(170, 140, 150, 80));
 
         jLabel3.setIcon(new javax.swing.ImageIcon(getClass().getResource("/images/Negro_Trans_2.png"))); // NOI18N
@@ -131,163 +114,6 @@ public class GestionContenidoWindow extends javax.swing.JFrame {
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
-
-    private void jComboBox1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jComboBox1ActionPerformed
-        String selectedAction = (String) jComboBox1.getSelectedItem();
-        if(null != selectedAction)switch (selectedAction) {
-            case "..." -> {
-                jCheckBox_Anadir.setEnabled(false);
-                jCheckBox_Editar.setEnabled(false);
-                jCheckBox_Borrar.setEnabled(false);
-            }
-            case "Pelicula" -> {
-                jCheckBox_Anadir.setEnabled(true);
-                jCheckBox_Editar.setEnabled(true);
-                jCheckBox_Borrar.setEnabled(true);
-            }
-            case "Serie" -> {
-                jCheckBox_Anadir.setEnabled(true);
-                jCheckBox_Editar.setEnabled(true);
-                jCheckBox_Borrar.setEnabled(true);
-            }
-            case "Actor" -> {
-                jCheckBox_Anadir.setEnabled(true);
-                jCheckBox_Editar.setEnabled(true);
-                jCheckBox_Borrar.setEnabled(true);
-            }
-            case "Director" -> {
-                jCheckBox_Anadir.setEnabled(true);
-                jCheckBox_Editar.setEnabled(true);
-                jCheckBox_Borrar.setEnabled(true);
-            }
-            case "Estudio" -> {
-                jCheckBox_Anadir.setEnabled(true);
-                jCheckBox_Editar.setEnabled(true);
-                jCheckBox_Borrar.setEnabled(true);
-            }
-            default -> {
-            }
-        }
-    }//GEN-LAST:event_jComboBox1ActionPerformed
-
-    private void jCheckBox_EditarMouseClicked(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_jCheckBox_EditarMouseClicked
-        if(!jCheckBox_Editar.isEnabled()){
-            
-        } else if(jCheckBox_Editar.isSelected() && jCheckBox_Editar.isEnabled()){
-            jCheckBox_Anadir.setEnabled(false);
-            jCheckBox_Borrar.setEnabled(false);
-        } else if(!jCheckBox_Editar.isSelected()){
-            jCheckBox_Anadir.setEnabled(true);
-            jCheckBox_Borrar.setEnabled(true);
-        }
-    }//GEN-LAST:event_jCheckBox_EditarMouseClicked
-
-    private void jCheckBox_AnadirMouseClicked(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_jCheckBox_AnadirMouseClicked
-        if(!jCheckBox_Anadir.isEnabled()){
-            
-        } else if(jCheckBox_Anadir.isSelected() && jCheckBox_Anadir.isEnabled()){
-            jCheckBox_Editar.setEnabled(false);
-            jCheckBox_Borrar.setEnabled(false);
-        } else if(!jCheckBox_Anadir.isSelected()){
-            jCheckBox_Editar.setEnabled(true);
-            jCheckBox_Borrar.setEnabled(true);
-        }
-    }//GEN-LAST:event_jCheckBox_AnadirMouseClicked
-
-    private void jCheckBox_BorrarMouseClicked(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_jCheckBox_BorrarMouseClicked
-        if(!jCheckBox_Borrar.isEnabled()){
-            
-        } else if(jCheckBox_Borrar.isSelected() && jCheckBox_Borrar.isEnabled()){
-            jCheckBox_Editar.setEnabled(false);
-            jCheckBox_Anadir.setEnabled(false);
-        } else if(!jCheckBox_Borrar.isSelected()){
-            jCheckBox_Editar.setEnabled(true);
-            jCheckBox_Anadir.setEnabled(true);
-        }
-    }//GEN-LAST:event_jCheckBox_BorrarMouseClicked
-
-    private void jButton1MouseClicked(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_jButton1MouseClicked
-        String selectedAction = (String) jComboBox1.getSelectedItem();
-        int accion; // 0 = Añadir | 1 = Editar | 2 = Borrar | 3 = No seleccionado
-        
-        if(jCheckBox_Anadir.isEnabled()){
-            accion = 0;
-        } else if(jCheckBox_Editar.isEnabled()){
-            accion = 1;
-        } else if(jCheckBox_Borrar.isEnabled()){
-            accion = 2;
-        } else {
-            accion = 3;
-        }
-        
-        if(null != selectedAction)switch (selectedAction) {
-            case "..." -> {
-                
-            }
-            case "Pelicula" -> {
-                if(accion == 0){
-                    dispose();
-                    new AnadirPeliculaWindow().setVisible(true);
-                } else if (accion == 1){
-                    dispose();
-                    new EditarPeliculaWindow().setVisible(true);
-                } else if (accion == 2){
-                    dispose();
-                    new BorrarPeliculaWindow().setVisible(true);
-                }
-            }
-            case "Serie" -> {
-                if(accion == 0){
-                    dispose();
-                    new AnadirSerieWindow().setVisible(true);
-                } else if (accion == 1){
-                    dispose();
-                    new EditarSerieWindow().setVisible(true);
-                } else if (accion == 2){
-                    dispose();
-                    new BorrarSerieWindow().setVisible(true);
-                }
-            }
-            case "Actor" -> {
-                if(accion == 0){
-                    dispose();
-                    new AnadirActorWindow().setVisible(true);
-                } else if (accion == 1){
-                    dispose();
-                    new EditarActorWindow().setVisible(true);
-                } else if (accion == 2){
-                    dispose();
-                    new BorrarActorWindow().setVisible(true);
-                }
-            }
-            case "Director" -> {
-                if(accion == 0){
-                    dispose();
-                    new AnadirDirectorWindow().setVisible(true);
-                } else if (accion == 1){
-                    dispose();
-                    new EditarDirectorWindow().setVisible(true);
-                } else if (accion == 2){
-                    dispose();
-                    new BorrarDirectorWindow().setVisible(true);
-                }
-            }
-            case "Estudio" -> {
-                if(accion == 0){
-                    dispose();
-                    new AnadirEstudioWindow().setVisible(true);
-                } else if (accion == 1){
-                    dispose();
-                    new EditarEstudioWindow().setVisible(true);
-                } else if (accion == 2){
-                    dispose();
-                    new BorrarEstudioWindow().setVisible(true);
-                }
-            }
-            default -> {
-            }}
-        
-    }//GEN-LAST:event_jButton1MouseClicked
 
     /**
      * @param args the command line arguments
@@ -326,11 +152,11 @@ public class GestionContenidoWindow extends javax.swing.JFrame {
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
-    private javax.swing.JButton jButton1;
-    private javax.swing.JCheckBox jCheckBox_Anadir;
-    private javax.swing.JCheckBox jCheckBox_Borrar;
-    private javax.swing.JCheckBox jCheckBox_Editar;
-    private javax.swing.JComboBox<String> jComboBox1;
+    public javax.swing.JButton jButton1;
+    public javax.swing.JCheckBox jCheckBox_Anadir;
+    public javax.swing.JCheckBox jCheckBox_Borrar;
+    public javax.swing.JCheckBox jCheckBox_Editar;
+    public javax.swing.JComboBox<String> jComboBox1;
     private javax.swing.JLabel jLabel1;
     private javax.swing.JLabel jLabel2;
     private javax.swing.JLabel jLabel3;
